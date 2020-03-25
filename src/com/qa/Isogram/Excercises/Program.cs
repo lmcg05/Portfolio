49df16229881938218e7dba54be8dfb8ab1eb4ae
Your Name <you@example.com>
2020-03-25 19:41:28 +0000

    Isogram And SmoothSentences Added

---
+++ src/com/qa/Isogram/Excercises/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lol1 = "Hello";
+            Console.WriteLine(Isisogram(lol1));
+        }
+        static bool Isisogram(string lol)
+        {
+
+            lol = lol.ToLower(); 
+            int len = lol.Length;
+            char[] array = lol.ToCharArray();
+            Array.Sort(array);
+            for (int i = 0; i < len - 1; i++)
+            {
+                if (array[i] == array[i + 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
